69a3b61c2e087c05e87e19086e8ca1e443ba44cb
shwetank <shwetanksinghsrmscet@gmail.com>
2020-03-29 14:30:17 +0530

    First Commit, Initial Setup.

---
+++ main.cc
@@ -0,0 +1,36 @@
+
+#include <cstdio>
+#include <iostream>
+#include <ctime>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <cstring>
+#include <set>
+#include <map>
+#include <cstdlib>
+#include <ctime>
+#include <cassert>
+#include <bitset>
+#include <deque>
+#include <stack>
+#include <climits>
+#include <string>
+#include <queue>
+#include <memory.h>
+#include <unordered_map>
+#include <random>
+
+#define  MAX 1000000007
+#define FROM_FILE false
+using namespace std;
+
+
+int main() {
+	ifstream ptrfile("ip");
+	istream& infile = FROM_FILE ? ptrfile : cin;
+	
+	return 0;
+}
